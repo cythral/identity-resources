0699b74a1660ec08ad5db9ae9bc1c49511a5b802
Brighid <52382196+brighid-bot@users.noreply.github.com>
2021-11-17 17:13:48 -0600

    Update lambdajection monorepo to v0.9.0-beta3 (#6)
    Co-authored-by: Talen Fisher <talen.fisher@cythral.com>

--- scripts/create-config-files.sh
+++ scripts/create-config-files.sh
@@ -4,10 +4,26 @@ cwd=$(dirname "${BASH_SOURCE[0]}")
 
 version=$CODEBUILD_BUILD_NUMBER
 
+get_lambdajection_version()
+{
+    pushd $cwd/../ >/dev/null
+    dotnet list package | grep Lambdajection | head -n 1 | awk '{ print $4 }'
+    popd >/dev/null
+}
+
+get_dotnet_version()
+{
+    pushd $cwd/../ >/dev/null
+    dotnet --version
+    popd >/dev/null
+}
+
 for file in $(ls ${cwd}/../deploy/params/*.json); do
     envName=$(echo $file | xargs basename | sed "s/\.json//")
     params=$(cat $file)
-    
+    params=$(echo $params | jq ".LambdajectionVersion=\"$(get_lambdajection_version)\"")
+    params=$(echo $params | jq ".DotnetVersion=\"$(get_dotnet_version)\"")
+
     config=$(cat $cwd/../deploy/config.json)
     config=$(echo $config | jq --argjson params "$params" '.Parameters=$params')
     echo $config > identity-resources.${envName}.config.json
